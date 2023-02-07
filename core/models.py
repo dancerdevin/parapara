e6ad9d0cee8e453ba54e875b6accae14ef58df06
dancerdevin <devinrfitzpatrick@gmail.com>
2023-02-07 11:39:41 -0600

    It worked! The strings are supposed to point toward the class, so they needed to be singular, and I was pluralizing them. Thank you, Daniel!

--- core/models.py
+++ core/models.py
@@ -29,7 +29,7 @@ class User(models.Model):
         null=False
     )
     characters = models.ForeignKey(
-        'core.Characters',
+        'Character',
         on_delete=models.CASCADE
     )
     display_name = models.CharField(
@@ -52,15 +52,15 @@ class Character(models.Model):
     # some or all of their Paradigm or Parameter data with them (or to make implementing that functionality easier).
     # And if a User wants to start completely from scratch without deleting any data, they can make a new Character.
     arcs = models.ForeignKey(
-        'core.Arcs',
+        'Arc',
         on_delete=models.CASCADE
     )
     paradigms = models.ForeignKey(
-        'core.Paradigms',
+        'Paradigm',
         on_delete=models.CASCADE
     )
     parameters = models.ForeignKey(
-        'core.Parameters',
+        'Parameter',
         on_delete=models.CASCADE
     )
 
@@ -71,7 +71,7 @@ class Arc(models.Model):
     # then like chapters or paragraphs of a story, and so belong to an Arc. But the "stats," e.g., Paradigms and
     # Parameters, belong to the Character, not necessarily any particular Arc.
     event_logs = models.ForeignKey(
-        'core.EventLogs',
+        'EventLog',
         on_delete=models.CASCADE
     )
     description = models.CharField(
@@ -99,7 +99,7 @@ class Paradigm(models.Model):
         null=True
     )
     default_parameters = models.ManyToManyField(
-        'core.Parameters'
+        'Parameter'
     )
     level = models.IntegerField(
         'Experience level of this Paradigm',
@@ -171,12 +171,12 @@ class EventLog(models.Model):
         null=False
     )
     tagged_paradigms = models.ManyToManyField(
-        'core.Paradigms',
+        'Paradigm',
         # Determines which Paradigms and default_parameters gain XP
         blank=True
     )
     tagged_parameters = models.ManyToManyField(
-        'core.py Parameters',
+        'Parameter',
         # Determines which Parameters gain XP, additive on any XP from default_parameters of tagged Paradigms
         blank=True
     )
