c839659f9b0a3491139f1aa43a61ca6e5036f3d9
dancerdevin <devinrfitzpatrick@gmail.com>
2023-03-11 16:59:55 -0600

    Registering the six core models in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Character, Arc, Paradigm, Parameter, EventLog
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Character)
+admin.site.register(Arc)
+admin.site.register(Paradigm)
+admin.site.register(Parameter)
+admin.site.register(EventLog)
